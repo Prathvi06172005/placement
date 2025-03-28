f4790f9d4dac82fa7366e0145a56f73dbc0c1746
Prathvi06172005 <prathvi.23cs103@sode-edu.in>
2025-03-28 08:31:55 +0530

    Update Prg10.java

--- Prg10.java
+++ Prg10.java
@@ -3,7 +3,7 @@ public static void main (String[]args){
 int n=5;
 for(int i=0;i<n;i++){
 for(int j=0;j<n;j++){
-if(i==0||j==n/2||(i==n-1&&j<=n/2)||(j==0&&j<0))
+if(i==0||j==n/2||i==n-1&&j>n/2||j==0&&i>n/2)
 {
 System.out.print("*"+" ");
 }
